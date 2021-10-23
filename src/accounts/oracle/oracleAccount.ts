f6bb7a96610f829417821eb84a8f1266e1d0927e
Conner Gallagher <ConnerNGallagher@gmail.com>
2021-10-22 21:30:05 -0600

    added KEYPAIR_OUTPUT env variable

--- src/accounts/oracle/oracleAccount.ts
+++ src/accounts/oracle/oracleAccount.ts
@@ -5,6 +5,7 @@ import {
 import * as anchor from "@project-serum/anchor";
 import { writePublicKey } from "../../utils/writePublicKey";
 import { readPublicKey } from "../../utils/readPublicKey";
+import { writeSecretKey } from "../../utils/writeSecretKey";
 
 export const getOracleAccount = async (
   program: anchor.Program,
@@ -12,26 +13,30 @@ export const getOracleAccount = async (
 ): Promise<OracleAccount> => {
   // try to read file, if not found create
   const fName = "oracle_account";
-  const readKey = readPublicKey(fName);
-  if (readKey) {
-    try {
-      const oracleAccount = new OracleAccount({
-        program,
-        publicKey: readKey,
-      });
-      console.log(`loaded ${fName} from local storage`);
-      return oracleAccount;
-    } catch (err) {
-      console.error(err);
-    }
-  }
+  // const readKey = readPublicKey(fName);
+  // if (readKey) {
+  //   try {
+  //     const oracleAccount = new OracleAccount({
+  //       program,
+  //       publicKey: readKey,
+  //     });
+  //     console.log(`loaded ${fName} from local storage`);
+  //     return oracleAccount;
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }
 
   const oracleAccount = await OracleAccount.create(program, {
     queueAccount,
   });
   if (oracleAccount?.publicKey) {
-    console.log(`saving ${fName}`);
+    console.log(`saving ${fName} public key`);
     writePublicKey(fName, oracleAccount?.publicKey);
+  }
+  if (oracleAccount?.keypair) {
+    console.log(`saving ${fName} keypair`);
+    writeSecretKey(fName, oracleAccount?.keypair);
   }
   console.log(`created ${fName}`);
   return oracleAccount;
--- src/types/const.ts
+++ src/types/const.ts
@@ -6,5 +6,5 @@ export const RPC_URL = process.env.RPC_URL
   : "https://api.devnet.solana.com";
 
 export const KEYPAIR_OUTPUT = process.env.KEYPAIR_OUTPUT
-  ? process.env.KEYPAIR_OUTPUT
+  ? `keypairs-${process.env.KEYPAIR_OUTPUT}` // use prefix for gitignore glob pattern
   : "keypairs";
