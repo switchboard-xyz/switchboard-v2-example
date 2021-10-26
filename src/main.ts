1cce575751383daf990cc1878ecaadd0b82f9b30
Conner Gallagher <ConnerNGallagher@gmail.com>
2021-10-26 09:54:16 -0600

    added prompts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { OracleQueue } from "./accounts";
 import { OracleQueueDefinition, OracleQueueSchema } from "./types";
 import fs from "fs";
-import { OracleQueue } from "./accounts";
+import prompts from "prompts";
 
 async function queueInit(): Promise<void> {
   const fileBuffer = fs.readFileSync("input.json");
@@ -9,14 +10,55 @@ async function queueInit(): Promise<void> {
   );
 
   // check if output file exists
-  if (fs.existsSync("output.json")) return;
-
-  const oracleQueue = new OracleQueue(queueDefinition);
-  const oracleQueueSchema = await oracleQueue.create();
-  console.log(oracleQueueSchema);
-
-  fs.writeFileSync("output.json", JSON.stringify(oracleQueueSchema, null, 2));
-  console.log("schema", oracleQueueSchema.keypair);
+  const outFile = "output.json";
+  let queueSchemaDefinition: OracleQueueSchema;
+  if (fs.existsSync(outFile)) {
+    const fileBuffer = fs.readFileSync(outFile);
+    queueSchemaDefinition = JSON.parse(fileBuffer.toString());
+    console.log("Oracle Queue built from local schema");
+  } else {
+    const oracleQueue = new OracleQueue(queueDefinition);
+    queueSchemaDefinition = await oracleQueue.create();
+    fs.writeFileSync(
+      "output.json",
+      JSON.stringify(queueSchemaDefinition, null, 2)
+    );
+    console.log("Oracle Queue built");
+  }
+  const answer = await prompts([
+    {
+      type: "select",
+      name: "action",
+      message: "what do you want to do?",
+      choices: [
+        {
+          title: "1. Verify Job Definitions",
+          value: "verifyJobs",
+        },
+        {
+          title: "2. Oracle Heartbeat",
+          value: "oracleHeartbeat",
+        },
+        {
+          title: "3. Request Aggregator Update",
+          value: "aggregatorUpdate",
+        },
+        {
+          title: "4. Read Aggregator Result",
+          value: "aggregatorResult",
+        },
+        {
+          title: "5. Turn the Crank",
+          value: "crankTurn",
+        },
+        {
+          title: "6. Read the Crank",
+          value: "readCrank",
+        },
+      ],
+    },
+  ]);
+  console.log("selected:", answer.action);
 }
 
 queueInit().then(
