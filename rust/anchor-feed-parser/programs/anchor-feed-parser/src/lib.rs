0a4a0a72bc3501267d9dbc7a28752be1cf2ae4d2
mgild <mgild@users.noreply.github.com>
2021-11-01 22:51:34 -0400

    startoff

--- rust/anchor-feed-parser/programs/anchor-feed-parser/src/lib.rs
+++ rust/anchor-feed-parser/programs/anchor-feed-parser/src/lib.rs
@@ -1,7 +1,7 @@
 #[allow(unaligned_references)]
 use anchor_lang::prelude::*;
 use std::convert::TryInto;
-use switchboard_aggregator::get_aggregator_result;
+use switchboard_aggregator::structs::{AggregatorAccountData, AggregatorRound, SwitchboardDecimal};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -19,11 +19,16 @@ pub mod anchor_feed_parser {
     use super::*;
 
     pub fn read_result(ctx: Context<ReadResult>, _params: ReadResultParams) -> ProgramResult {
-        let aggregator = &ctx.accounts.aggregator;
-        let aggregator_result = &get_aggregator_result(aggregator)?.result;
-        let final_result: u64 = aggregator_result.try_into()?;
-
-        msg!("Current feed result is {}!", final_result);
+        msg!("{}", std::mem::size_of::<AggregatorAccountData>());
+        msg!("a");
+        let buf = ctx.accounts.aggregator.try_borrow_data()?;
+        // let aggregator = AggregatorAccountData::new(&ctx.accounts.aggregator)?;
+        let aggregator: &AggregatorAccountData = bytemuck::from_bytes(&buf[8..]);
+        // msg!("a");
+        // aggregator.get_result()
+        // let final_result: u64 = aggregator_result.try_into()?;
+//
+        // msg!("Current feed result is {}!", final_result);
         Ok(())
     }
 }
