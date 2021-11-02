071d6450c6f9cabb1a407f139c8c3827ca9ed60c
mgild <mgild@users.noreply.github.com>
2021-11-01 23:02:01 -0400

    example using loader

--- rust/anchor-feed-parser/programs/anchor-feed-parser/src/lib.rs
+++ rust/anchor-feed-parser/programs/anchor-feed-parser/src/lib.rs
@@ -1,6 +1,7 @@
 #[allow(unaligned_references)]
 use anchor_lang::prelude::*;
 use std::convert::TryInto;
+use std::str::FromStr;
 use switchboard_aggregator::structs::{AggregatorAccountData, AggregatorRound, SwitchboardDecimal};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
@@ -23,7 +24,11 @@ pub mod anchor_feed_parser {
         msg!("a");
         let buf = ctx.accounts.aggregator.try_borrow_data()?;
         // let aggregator = AggregatorAccountData::new(&ctx.accounts.aggregator)?;
-        let aggregator: &AggregatorAccountData = bytemuck::from_bytes(&buf[8..]);
+        // let aggregator: &AggregatorAccountData = bytemuck::from_bytes(&buf[8..]);
+        let pid = solana_program::pubkey::Pubkey::from_str("5n43jDh58UzjqGE2sFuZPrkgx52BT6PWgcdL1CvBU9Ww").unwrap();
+        let aggregator_account_loader =
+            Loader::<AggregatorAccountData>::try_from(&pid, &ctx.accounts.aggregator)?;
+        let aggregator: AggregatorAccountData = *aggregator_account_loader.load()?;
         // msg!("a");
         // aggregator.get_result()
         // let final_result: u64 = aggregator_result.try_into()?;
